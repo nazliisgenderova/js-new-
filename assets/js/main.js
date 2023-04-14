c101ae9251ba0f4206b9537467f00fa64db024cc
nazliisgenderova <7nt5ajk@code.edu.az>
2023-04-14 16:55:27 +0400

    test

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+// task-4
+
+
+// var num1=Number(promt("Brinci ededi daxil edin:"))
+// var num2=Number(promt("Ikinci daxil edin:"));
+// var sum=0;
+// for(var i=num1+1; i<num2; i++){
+//    sum+=i
+// }
+// alert(sum)
+
+// task-5
+
+// var num=Number(promt("eded daxil din:"));
+// var sum =0;
+// var count=0;
+
+// while(num>0){
+//     var digit=num%10;
+//     num=num-digit;
+//     num=num/10;
+
+//     sum+=digit;
+//     count++;
+// }
+
+// var avg=sum/count;
+// alert(avg);
+
